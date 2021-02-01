232ceb96927088c353b47e1628ac76f4a4cfb564
Fedor Nikolaev <nikolaevfo@gmail.com>
2021-02-01 14:44:04 +0400

    feat: douwnload user info

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,26 +1,42 @@
+import React from 'react';
+import api from '../utils/api';
+
 function Main(props) {
+
+  const [userName, setUserName] = React.useState('');
+  const [userDescription, setUserDescription] = React.useState('');
+  const [userAvatar, setUserAvatar] = React.useState('');
+
+  React.useEffect(() => {
+    api.douwnloadUserInfo()
+      .then((userData) => {
+        setUserName(userData.name);
+        setUserDescription(userData.about);
+        setUserAvatar(userData.avatar);
+      });
+  })
 
   return (
     <main className="content">
 
       <section className="profile">
         <div className="profile__avatar-container" onClick={props.onEditAvatar}>
-          <img src="#" alt="Аватар" className="profile__avatar" />
+          <div className="profile__avatar" style={{ background: `url(${userAvatar})`, backgroundSize: 'cover' }}></div>
           <div className="profile__avatar-bg"></div>
         </div>
         <div className="profile__info">
           <div className="profile__info-row">
-            <h1 className="profile__title"></h1>
+            <h1 className="profile__title">{userName}</h1>
             <button type="button" className="profile__edit-button button" onClick={props.onEditProfile}></button>
           </div>
-          <p className="profile__subtitle"></p>
+          <p className="profile__subtitle">{userDescription}</p>
         </div>
         <button type="button" className="profile__add-button button" onClick={props.onAddPlace}></button>
       </section>
 
       <section className="elements"> </section>
 
-    </main>
+    </main >
 
   );
 }
